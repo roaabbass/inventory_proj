02770387aa2a641338cb446e8f4b5406f15d6017
Roaa <roaabbass@gmail.com>
2025-05-19 20:19:21 +0300

    app upgraded

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
-from flask import Flask , render_templetes, request , redirect , url_for ,flash
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager , UserMixin, login_user,login_required,logout_
+from flask import Flask , render_template
+
+#import db
+
+app = Flask(__name__)
+
+@app.route("/") 
+def home():
+    return"<h2>Welcome To Our Inventory Manager!! </h2>"
+if __name__=="__main__":
+    app.run(debug = True, port=5050)
+
+
+
